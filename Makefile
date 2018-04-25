9678c4ff0692a7ddb7b4a5e0136d28a671c74607
Matthias Luebken <forname dot lastname at gmail dot com>
2018-04-25 10:10:14 +0200

    k8s

--- Makefile
+++ Makefile
@@ -1,16 +1,34 @@
 #! /usr/bin/make -f
 
 .DEFAULT_GOAL := help
+MY_VAR := $(shell kubectl get -o json svc/simpleservice-service |jq .status.loadBalancer.ingress[0].ip)
 
 build: ## build the jar and docker image
 	./mvnw package
 	docker build -t luebken/simpleservice .
 
+watch-kubectl:
+	watch -n 1 kubectl get svc,deploy,rs,po
+
 java-run: ## run the jar
-	java -jar target/simpleservice-0.0.1.jar
+	export SIMPLE_SERVICE_DOWNSTREAM_SERVICE=http://localhost:8080; java -jar target/simpleservice-0.0.1.jar
 
 docker-push: ## push the docker image
 	docker push luebken/simpleservice
+
+kubectl-create: # creates a svc and deploy
+	kubectl create -f deployment.yaml
+	kubectl create -f service.yaml
+
+kubectl-apply: # updates a deploy
+	kubectl apply -f deployment.yaml
+
+kubectl-delete:
+	-kubectl delete svc/simpleservice-service
+	kubectl delete deploy/simpleservice-deploy
+
+curl:
+	curl $(MY_VAR)
 
 # via http://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
 help: ##Shows help message
--- src/main/java/simpleservice/Application.java
+++ src/main/java/simpleservice/Application.java
@@ -5,10 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
+import org.springframework.http.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 @RestController
 public class Application {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/")
     public String home() {
@@ -21,12 +34,22 @@ public class Application {
 
         HashMap<String, Object> envMap = new HashMap<>();
         envMap.put("SIMPLE_SERVICE_VERSION", System.getenv("SIMPLE_SERVICE_VERSION"));
+        envMap.put("SIMPLE_SERVICE_DOWNSTREAM_SERVICE", System.getenv("SIMPLE_SERVICE_DOWNSTREAM_SERVICE"));
         Map<String, String> env = System.getenv();
         for (String envName : env.keySet()) {
             envMap.put(envName, env.get(envName));
         }
         root.put("env", envMap);
-        
+
+        // Check & call downstream service
+        String downstream = System.getenv("SIMPLE_SERVICE_DOWNSTREAM_SERVICE");
+        if (downstream != null && !downstream.trim().isEmpty()) {
+            HttpHeaders headers = new HttpHeaders();
+            //headers.set(key, parameters.get(key));
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            ResponseEntity<String> response = restTemplate.exchange(downstream, HttpMethod.GET, entity, String.class);
+            root.put(downstream, response.getBody());
+        }
         return root;
     }
 
